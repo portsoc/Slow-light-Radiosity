522ae1b4d56e9303e231636204b90bcb99f8f9a5
Jacek Kopecky <jacek@jacek.cz>
2020-04-08 10:21:08 +0100

    simplifying
    Co-Authored-By: Thomas Mattone <Eccsx@users.noreply.github.com>

--- radiosity/point3.js
+++ radiosity/point3.js
@@ -5,16 +5,10 @@ class Point3 {
     this.z = z;
   }
 
-  // object's functions
-  addVector3(v) {
+  addVector(v) {
     this.x += v.x;
     this.y += v.y;
     this.z += v.z;
-  }
-
-  // static function
-  static addVector3ToPoint3(p, v) {
-    return new Point3(v.x + p.x, v.y + p.y, v.z + p.z);
   }
 }
 
