3c4a2edd52b838530f2eb1185985e91453c29d9f
Thomas <cristalisation314@gmail.com>
2020-04-08 18:44:12 +0200

    :sparkles: patch3 class

---
+++ radiosity/patch3.js
@@ -0,0 +1,24 @@
+import Element3 from "./element3"
+import Point3 from './point3.js';
+
+export default class Patch3 extends Element3 {
+    constructor(vertex, surface) {
+        super(vertex, null);
+        this.elem = null;
+        this.surface = surface;
+    }
+
+    get unsentFlux() {
+        return (this.exitance.GetRedBand() + this.exitance.GetGreenBand() + this.exitance.GetBlueBand()) * this.area;
+    }
+
+    get center() {
+        let cv = new Vector3(0, 0, 0);
+        for (let i = 0; i < this.numVert; i++) {
+            cv.add(new Vector3(this.vertexArray[i].posn))
+        }
+        cv.scale(1 / this.numVert);
+        this.center = new Point3(cv.x, cv.y, cv.z);
+        return this.center;
+    }
+}
