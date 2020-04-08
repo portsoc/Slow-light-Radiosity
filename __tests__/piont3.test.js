36503a457c8f16e9e7bc77ac15625eed2bc1de98
Thomas <cristalisation314@gmail.com>
2020-04-08 14:14:09 +0200

    :heavy_check_mark: point3 tests

---
+++ __tests__/piont3.test.js
@@ -0,0 +1,8 @@
+import Vector3 from '../radiosity/vector3.js';
+import Point3 from '../radiosity/point3.js';
+
+test('addVector()', () => {
+  const p = new Point3(-4, 2, -7);
+  const v = new Vector3(1, 2, 3);
+  expect(p.addVector(v)).toMatchObject(new Point3(-3, 4, -4));
+});
--- radiosity/point3.js
+++ radiosity/point3.js
@@ -9,5 +9,6 @@ export default class Point3 {
     this.x += v.x;
     this.y += v.y;
     this.z += v.z;
+    return this;
   }
 }
