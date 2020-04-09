f1e18a0bd2aed1bb70c5735bb3180a7764d214af
Thomas <cristalisation314@gmail.com>
2020-04-09 14:36:03 +0200

    :hammer: vertex3 class elemlist change

--- radiosity/vertex3.js
+++ radiosity/vertex3.js
@@ -2,13 +2,13 @@ export default class Vertex3 {
   constructor(coord) {
     this.posn = coord;       // Vexter co-ordinates
     this.normal = 0;         // Vertex normal
-    this.elemList = null;    // Element list head
+    this.elemList = [];    // Element list head
     this.next = null;        // Next vertex
     this.exitance.reset();   // Vertex exitance
   }
 
   get normal() {
-    while (this.elemList != null) { // * null or undefined
+    while (this.elemList === []) {
       this.normal += this.elemList.elem.normal;
       this.elemList = this.elemList.next;
     }
