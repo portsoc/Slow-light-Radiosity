927e4c7f30909b270b7f2faa4712cfb12f8a4d4c
Thomas <cristalisation314@gmail.com>
2020-04-09 14:46:56 +0200

    :hammer: remove field flag in element3 class
    - replaced with a boolean field isQuad

--- radiosity/element3.js
+++ radiosity/element3.js
@@ -4,22 +4,14 @@ export default class Element3 {
   constructor(vertexArray, patch) {
     this.patch = patch;               // Parent patch
     this.area = 0;                    // Element area
-    this.flags = 0;                   // Falgs bitmap
     this.next = null;                 // Next element
     this.exitance.reset();            // Spectral exitance
     this.vertexArray = vertexArray;   // Vexter array
-  }
-
-  isQuad() {
-    return this.flags & 0x01;
+    this.isQuad = false;
   }
 
   get numVert() {
-    return (this.flags & 0x01) ? 4 : 3;
-  }
-
-  setQuad() {
-    this.flag |= 0x01;
+    return this.isQuad ? 4 : 3;
   }
 
   get area() {
@@ -27,7 +19,7 @@ export default class Element3 {
     const vb = new Vector3(this.vertexArray[0].posn, this.vertexArray[2].posn);
     let temp = va.cross(vb);
     this.area = temp.length / 2.0;
-    if (this.isQuad()) {
+    if (this.isQuad) {
       const vc = new Vector3(this.vertexArray[3].posn, this.vertexArray[0].posn);
       temp = vb.cross(vc);
       this.area += temp.length / 2.0;
