f22e70a09d738029c8843e262e7389b62e0876ad
Thomas <cristalisation314@gmail.com>
2020-04-09 15:27:20 +0200

    :hammer: revisit get area() in element3 class

--- radiosity/element3.js
+++ radiosity/element3.js
@@ -4,7 +4,7 @@ import Spectra from './spectra.js';
 export default class Element3 {
   constructor(vertexArray, patch) {
     this.patch = patch;               // Parent patch
-    this.area = 0;                    // Element area
+    this._area = 0;                    // Element area
     this.next = null;                 // Next element
     this.exitance = new Spectra();            // Spectral exitance
     this.vertexArray = vertexArray;   // Vexter array
@@ -16,16 +16,18 @@ export default class Element3 {
   }
 
   get area() {
-    const va = new Vector3(this.vertexArray[0].posn, this.vertexArray[1].posn);
-    const vb = new Vector3(this.vertexArray[0].posn, this.vertexArray[2].posn);
-    let temp = va.cross(vb);
-    this.area = temp.length / 2.0;
-    if (this.isQuad) {
-      const vc = new Vector3(this.vertexArray[3].posn, this.vertexArray[0].posn);
-      temp = vb.cross(vc);
-      this.area += temp.length / 2.0;
+    if (this._area === 0) {
+      const va = new Vector3(this.vertexArray[0].posn, this.vertexArray[1].posn);
+      const vb = new Vector3(this.vertexArray[0].posn, this.vertexArray[2].posn);
+      let temp = va.cross(vb);
+      this._area = temp.length / 2.0;
+      if (this.isQuad) {
+        const vc = new Vector3(this.vertexArray[3].posn, this.vertexArray[0].posn);
+        temp = vb.cross(vc);
+        this._area += temp.length / 2.0;
+      }
     }
-    return this.area;
+    return this._area;
   }
 
   get normal() {
