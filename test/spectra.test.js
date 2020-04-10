2117aeb26e3927b56a1bbab1862a447c6ced3976
Thomas <cristalisation314@gmail.com>
2020-04-10 13:45:51 +0200

    :heavy_check_mark: spectral tests

---
+++ test/spectra.test.js
@@ -0,0 +1,23 @@
+import Spectra from '../radiosity/spectra.js';
+
+test('add()', () => {
+  const s1 = new Spectra(12, 0, 5);
+  const s2 = new Spectra(0, 23, 1);
+  expect(s1.add(s2)).toMatchObject(new Spectra(12, 23, 6));
+});
+
+test('sub()', () => {
+  const s1 = new Spectra(12, 0, 5);
+  const s2 = new Spectra(0, 23, 1);
+  expect(s1.sub(s2)).toMatchObject(new Spectra(12, -23, 4));
+});
+
+test('maxColor()', () => {
+  const s1 = new Spectra(12, 0, 5);
+  expect(s1.maxColor).toBe(12);
+});
+
+test('scale()', () => {
+  const s1 = new Spectra(12, 0, 5);
+  expect(s1.scale(5)).toMatchObject(new Spectra(60, 0, 25));
+});
