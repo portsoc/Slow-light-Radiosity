9e9ce9fc471faff4113af453f94a0fa86ad390ab
Thomas <cristalisation314@gmail.com>
2020-04-10 20:20:29 +0200

    :sparkles: environ class

---
+++ radiosity/environ.js
@@ -0,0 +1,10 @@
+export default class Environ {
+  constructor() {
+    this.instanceList = [];
+    this.numInst = 0;
+    this.numSurf = 0;
+    this.numPatch = 0;
+    this.numElem = 0;
+    this.numVert = 0;
+  }
+}
