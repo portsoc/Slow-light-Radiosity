4e792db37f752f3a89c7398a40b2edbe4b4af091
Thomas <cristalisation314@gmail.com>
2020-04-10 11:10:23 +0200

    :hammer: revisit get normal() in vexter3 class
    - fix operation mistakes

--- radiosity/vertex3.js
+++ radiosity/vertex3.js
@@ -1,19 +1,22 @@
 import Spectra from './spectra.js';
+import Vector3 from './vector3.js';
 
 export default class Vertex3 {
   constructor(coord) {
     this.posn = coord;               // Vexter co-ordinates
-    this.normal = 0;                 // Vertex normal
+    this._normal = null;             // Vertex normal (Vector3)
     this.elemList = [];              // Element list head
     this.next = null;                // Next vertex
     this.exitance = new Spectra();   // Vertex exitance
   }
 
   get normal() {
-    while (this.elemList === []) {
-      this.normal += this.elemList.elem.normal;
-      this.elemList = this.elemList.next;
+    if (this._normal === null) {
+      this._normal = new Vector3(0, 0, 0);
+      for (const e of this.elemList) {
+        this._normal.add(e.normal());
+      }
     }
-    return this.normal.norm();
+    return this._normal.norm();
   }
 }
