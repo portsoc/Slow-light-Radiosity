82d2a2d190c834b2265361fff9f4827be7c3250e
Thomas <cristalisation314@gmail.com>
2020-04-10 17:02:41 +0200

    :bug: fix center() in patch3 class

--- radiosity/patch3.js
+++ radiosity/patch3.js
@@ -7,6 +7,7 @@ export default class Patch3 extends Element3 {
     super(vertexArray, null);
     this.elem = [];           // Element list
     this.parentSurface = parentSurface;   // Parent surface
+    this._center = null;
   }
 
   get unsentFlux() {
@@ -14,12 +15,14 @@ export default class Patch3 extends Element3 {
   }
 
   get center() {
-    const cv = new Vector3(0, 0, 0);
-    for (let i = 0; i < this.numVert; i++) {
-      cv.add(new Vector3(this.vertexArray[i].posn));
+    if (this._center === null) {
+      const cv = new Vector3(0, 0, 0);
+      for (let i = 0; i < this.numVert; i++) {
+        cv.add(new Vector3(this.vertexArray[i].posn));
+      }
+      cv.scale(1 / this.numVert);
+      this._center = new Point3(cv.x, cv.y, cv.z);
     }
-    cv.scale(1 / this.numVert);
-    this.center = new Point3(cv.x, cv.y, cv.z);
-    return this.center;
+    return this._center;
   }
 }
