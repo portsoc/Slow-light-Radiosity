e61704ec79f7ecdb926c3b2c5fb6858512093c14
Jacek Kopecky <jacek@jacek.cz>
2020-04-11 10:27:23 +0100

    starting review of instance/surface/environment

--- radiosity/surface3.js
+++ radiosity/surface3.js
@@ -2,7 +2,6 @@ export default class Surface3 {
   constructor(reflectance, emittance) {
     this.reflectance = reflectance;   // Spectral reflectance
     this.emittance = emittance;       // Initial radiant exitance
-    this.patchList = [];              // Patch list
-    this.next = null;                 // Next surface
+    this.patches = [];                // Patches that make up the surface
   }
 }
