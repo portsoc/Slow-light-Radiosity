09cb384e3e52538228b5d0444842736862f274c0
Jacek Kopecky <jacek@jacek.cz>
2020-04-11 10:14:23 +0100

    :ok_hand: vertex reviewed

--- radiosity/vertex3.js
+++ radiosity/vertex3.js
@@ -1,22 +1,26 @@
-import Spectra from './spectra.js';
-import Vector3 from './vector3.js';
+import Spectra from './spectra';
+import Vector3 from './vector3';
 
 export default class Vertex3 {
   constructor(coord) {
-    this.posn = coord;               // Vexter co-ordinates (Point3)
-    this._normal = null;             // Vertex normal (Vector3)
-    this.elemList = [];              // Element list head
-    this.next = null;                // Next vertex
+    this.pos = coord;                // Vertex co-ordinates (Point3)
+    this._normal = null;             // Vertex normal (Vector3) (computed once in getter)
+    this.elements = [];              // Elements that use this vertex
     this.exitance = new Spectra();   // Vertex exitance
   }
 
   get normal() {
-    if (this._normal === null) {
+    if (this._normal == null) {
       this._normal = new Vector3(0, 0, 0);
-      for (const e of this.elemList) {
+      for (const e of this.elements) {
         this._normal.add(e.normal);
       }
+      this._normal.normalize();
     }
-    return this._normal.norm();
+    return this._normal;
+  }
+
+  addElement(e) {
+    if (!this.elements.includes(e)) this.elements.push(e);
   }
 }
