604f0c28cc2248e2faca6c243025681e7ba97913
Jacek Kopecky <jacek@jacek.cz>
2020-04-11 10:25:55 +0100

    :ok_hand: element and patch reviewed

--- radiosity/patch3.js
+++ radiosity/patch3.js
@@ -1,26 +1,26 @@
-import Element3 from './element3.js';
-import Point3 from './point3.js';
-import Vector3 from './vector3.js';
+import Element3 from './element3';
+import Point3 from './point3';
+import Vector3 from './vector3';
 
 export default class Patch3 extends Element3 {
-  constructor(vertexArray, parentSurface) {
-    super(vertexArray, null);
-    this.elem = [];           // Element list
+  constructor(vertices, parentSurface) {
+    super(vertices, null);
+    this.elements = [];                   // Elements that make up this patch
     this.parentSurface = parentSurface;   // Parent surface
-    this._center = null;
+    this._center = null;                  // Vertex centroid (computed once in getter)
   }
 
   get unsentFlux() {
-    return (this.exitance.redBand + this.exitance.greenBand + this.exitance.blueBand) * this.area;
+    return (this.exitance.r + this.exitance.g + this.exitance.b) * this.area;
   }
 
   get center() {
-    if (this._center === null) {
+    if (this._center == null) {
       const cv = new Vector3(0, 0, 0);
-      for (let i = 0; i < this.numVert; i++) {
-        cv.add(new Vector3(this.vertexArray[i].posn));
+      for (const vert of this.vertices) {
+        cv.add(new Vector3(vert.pos));
       }
-      cv.scale(1 / this.numVert);
+      cv.scale(1 / this.vertices.length);
       this._center = new Point3(cv.x, cv.y, cv.z);
     }
     return this._center;
