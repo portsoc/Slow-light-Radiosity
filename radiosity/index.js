21979835e4b341620976a60ebaf2322985b419eb
Jacek Kopecky <jacek@jacek.cz>
2020-04-13 18:43:28 +0100

    export all radiosity classes from index.js

---
+++ radiosity/index.js
@@ -0,0 +1,9 @@
+export { default as Element3 } from './element3.js';
+export { default as Environment } from './environment.js';
+export { default as Instance } from './instance.js';
+export { default as Patch3 } from './patch3.js';
+export { default as Point3 } from './point3.js';
+export { default as Spectra } from './spectra.js';
+export { default as Surface3 } from './surface3.js';
+export { default as Vector3 } from './vector3.js';
+export { default as Vertex3 } from './vertex3.js';
