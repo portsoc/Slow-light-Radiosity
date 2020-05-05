10b8f1df976c1af25f8c77f44c6b951f095fb9dc
Jacek Kopecky <jacek@jacek.cz>
2020-05-05 10:38:03 +0100

    review formclip test

--- test/radiosity/formclip.test.js
+++ test/radiosity/formclip.test.js
@@ -1,17 +1,11 @@
 import FormClip from '../../radiosity/formclip.js';
-import FormClipEdge from '../../radiosity/formclipedge.js';
-import Vector4 from '../../radiosity/vector4.js';
 import Patch3 from '../../radiosity/patch3.js';
 import Vertex3 from '../../radiosity/vertex3.js';
 import Point3 from '../../radiosity/point3.js';
 
 test('isFacingAway()', () => {
   // set up FormClip
-  const v = new Vector4(1, 2, 3, 4);
-  const fce = new FormClipEdge(v);
-  const fc = new FormClip(fce);
-
-  // ? In front of test
+  const fc = new FormClip(null); // no FormClipEdge necessary to test isFacingAway()
 
   /*
    * ^ y
@@ -22,58 +16,59 @@ test('isFacingAway()', () => {
    * p0 ----- p1  -> x
    */
 
-  // set FromClip's origin
-  const origin1 = new Point3(0.5, 0.5, -2);
-  fc.origin = origin1;
-
   // set up patch
-  let p0 = new Point3(0, 0, 0);
-  let p1 = new Point3(1, 0, 0);
-  let p2 = new Point3(1, 1, 0);
-  let p3 = new Point3(0, 1, 0);
-  let vertices = [
-    new Vertex3(p0),
-    new Vertex3(p1),
-    new Vertex3(p2),
-    new Vertex3(p3),
+  let points = [
+    new Point3(0, 0, 0),
+    new Point3(1, 0, 0),
+    new Point3(1, 1, 0),
+    new Point3(0, 1, 0),
   ];
+  let vertices = points.map(p => new Vertex3(p));
   const patch1 = new Patch3(vertices);
 
+  // origin below the patch
+  fc.origin = new Point3(0.5, 0.5, -2);
   expect(fc.isFacingAway(patch1)).toBe(true);
 
-  // ? Behind test
+  // origin still below, more to the side
+  fc.origin = new Point3(5, 5, -2);
+  expect(fc.isFacingAway(patch1)).toBe(true);
 
-  /*
+  // origin in the same plane as the patch, counts as facing away
+  fc.origin = new Point3(5, 5, 0);
+  expect(fc.isFacingAway(patch1)).toBe(true);
+
+  // origin above the patch
+  fc.origin = new Point3(0, 0, 1);
+  expect(fc.isFacingAway(patch1)).toBe(false);
+
+  /* different coordinates, patch facing towards -y
    *               ^ y
    *               |
-   *           p0 -|------ p3
-   *           /   |      /
-   *    z <---/----0     /
-   *         /    /     /
-   *       p1 -------- p2
+   *              p0 ------ p1
+   *              /|       /
+   *    z <------/-0      /
+   *            / /      /
+   *          p3 ------ p2
    *            /
    *           x
    */
 
-  // set FromClip's origin
-  const origin2 = new Point3(3, 2, 1.5);
-  fc.origin = origin2;
-
   // set up patch
-  p0 = new Point3(-1, 1, 1);
-  p1 = new Point3(1, 1, 1);
-  p2 = new Point3(1, 1, -1);
-  p3 = new Point3(-1, 1, -1);
-  vertices = [
-    new Vertex3(p0),
-    new Vertex3(p1),
-    new Vertex3(p2),
-    new Vertex3(p3),
+  points = [
+    new Point3(0, 1, 0),
+    new Point3(0, 1, -1),
+    new Point3(1, 1, -1),
+    new Point3(1, 1, 0),
   ];
+  vertices = points.map(p => new Vertex3(p));
   const patch2 = new Patch3(vertices);
 
+  fc.origin = new Point3(3, 2, 1);
+  expect(fc.isFacingAway(patch2)).toBe(true);
+
+  fc.origin = new Point3(3, 0, 1);
   expect(fc.isFacingAway(patch2)).toBe(false);
 });
 
-test('clip()', () => {
-});
+// clip() is tested through hemiclip
