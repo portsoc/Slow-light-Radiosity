fd55b1e5c8b6ffcd40cd2235648095c29712f5ab
Thomas <cristalisation314@gmail.com>
2020-05-11 06:12:01 +0200

    close window when select another menu item

--- frontend/menu.js
+++ frontend/menu.js
@@ -1,6 +1,10 @@
 function stats() {
   const s = document.getElementById('stats');
   if (s.style.display === 'none' || s.style.display === '') {
+    // close other window
+    document.getElementById('controls').style.display = 'none';
+    document.getElementById('env').style.display = 'none';
+
     s.style.display = 'block';
   } else {
     s.style.display = 'none';
@@ -10,6 +14,10 @@ function stats() {
 function controls() {
   const c = document.getElementById('controls');
   if (c.style.display === 'none' || c.style.display === '') {
+    // close other window
+    document.getElementById('stats').style.display = 'none';
+    document.getElementById('env').style.display = 'none';
+
     c.style.display = 'block';
   } else {
     c.style.display = 'none';
@@ -19,6 +27,10 @@ function controls() {
 function env() {
   const e = document.getElementById('env');
   if (e.style.display === 'none' || e.style.display === '') {
+    // close other window
+    document.getElementById('stats').style.display = 'none';
+    document.getElementById('controls').style.display = 'none';
+
     e.style.display = 'block';
   } else {
     e.style.display = 'none';
