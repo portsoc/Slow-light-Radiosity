19962aab900bf168d72c09923e92b4655a918f01
Thomas <cristalisation314@gmail.com>
2020-05-11 02:35:47 +0200

    :computer: :sparkles: add commands to menu

--- frontend/menu.js
+++ frontend/menu.js
@@ -6,3 +6,12 @@ function stats() {
     s.style.display = 'none';
   }
 }
+
+function controls() {
+  const c = document.getElementById('controls');
+  if (c.style.display === 'none' || c.style.display === '') {
+    c.style.display = 'block';
+  } else {
+    c.style.display = 'none';
+  }
+}
