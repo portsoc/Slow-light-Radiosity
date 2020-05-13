440dc56edde596ef87896e24bb30893719bb0671
Jacek Kopecky <jacek@jacek.cz>
2020-05-13 23:37:22 +0100

    add env 5 with a lamp

--- modeling/test-models/two-cubes.js
+++ modeling/test-models/two-cubes.js
@@ -3,6 +3,7 @@
 import * as Rad from '../../radiosity/index.js';
 import Transform3 from '../transform3.js';
 import * as Cube from '../cube.js';
+import * as Cylinder from '../cylinder.js';
 
 export function createTwoCubes(subdivision = 2) {
   const c1 = cube1(subdivision);
@@ -19,15 +20,29 @@ export function createTwoCubesInRoom(subdivision = 2, subPatches) {
   return new Rad.Environment([c1, c2, r]);
 }
 
+export function createCubeAndLampInRoom(subdivision = 2, subPatches) {
+  const c1 = lamp1(subdivision);
+  const c2 = cube2(subdivision, subPatches, 1);
+  const r = room(subdivision * 2, subPatches);
+
+  return new Rad.Environment([c1, c2, r]);
+}
+
+function lamp1(subdivision) {
+  const L = 1.6;
+  const retval = Cylinder.lampshade(6 * subdivision, 0.7, 0.4, 1, subdivision);
+  retval.surfaces[1].emittance.add(new Rad.Spectra(L, L, L));
+  retval.surfaces[0].emittance.add(new Rad.Spectra(L / 2, L / 2, L / 2));
+  return retval;
+}
+
 function cube1(subdivision, subPatches) {
   return Cube.unitCube(new Rad.Spectra(0.12, 0.81, 0.21), null, subdivision, subPatches);
 }
 
-function cube2(subdivision, subPatches) {
+// L is light intensity
+function cube2(subdivision, subPatches, L = 10) {
   const retval = Cube.unitCubeMultiSurface(subdivision, subPatches);
-
-  // light intensity
-  const L = 10;
 
   retval.surfaces[0].reflectance.add(new Rad.Spectra(0.7, 0.2, 0.2));
   retval.surfaces[1].reflectance.add(new Rad.Spectra(0.7, 0.7, 0.2));
