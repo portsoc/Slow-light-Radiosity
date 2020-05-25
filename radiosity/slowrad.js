5d01167ea35915eaee5470b538aa77e71fe80266
Eccsx <cr>
2020-05-25 19:31:53 +0200

    :fire: remove ambient stuff from SlowRad

--- radiosity/slowrad.js
+++ radiosity/slowrad.js
@@ -7,11 +7,6 @@ export default class SlowRad {
     this.maxTime = maxTime;                    // Maximum number of steps
     this.env = null;                           // Environment
 
-    this.ambient = new Array(maxTime);         // Ambient future exitances
-    for (let i = 0; i < maxTime; i += 1) {
-      this.ambient[i] = new Spectra();
-    }
-
     this.irf = new Spectra();                  // Interreflection factors
     this.totalArea = 0;                        // Total patch area
 
@@ -45,8 +40,6 @@ export default class SlowRad {
   prepareForDisplay() {
     if (this.now < this.maxTime) {
       if (this.needsDisplayUpdate) {
-        this.calcAmbient();
-        this.env.ambient = this.ambient[this.now];
         this.env.interpolateVertexExitances(this.now);
         this.needsDisplayUpdate = false;
 
@@ -206,28 +199,6 @@ export default class SlowRad {
     this.irf.r = 1 / (1 - sum.r);
     this.irf.g = 1 / (1 - sum.g);
     this.irf.b = 1 / (1 - sum.b);
-
-    return this;
-  }
-
-  calcAmbient() {
-    const sum = new Spectra();
-    const tmp = new Spectra();
-
-    for (const patch of this.env.patches) {
-      // Update sum of unsent exitances times areas
-      tmp.setTo(patch.futureExitances[this.now]);
-      tmp.scale(patch.area);
-      sum.add(tmp);
-    }
-
-    // Calculate area-weighted average reflectance
-    sum.scale(1 / this.totalArea);
-
-    // Calculate interreflectance factors
-    this.ambient[this.now].r = this.irf.r * sum.r;
-    this.ambient[this.now].g = this.irf.g * sum.g;
-    this.ambient[this.now].b = this.irf.b * sum.b;
 
     return this;
   }
