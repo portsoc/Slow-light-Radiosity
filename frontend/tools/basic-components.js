c1643f69ef6d2f6d4e38169ec0ed862698268ccf
Jacek Kopecky <jacek@jacek.cz>
2020-05-25 15:03:25 +0100

    move document.querySelector into setupHtml

--- frontend/tools/basic-components.js
+++ frontend/tools/basic-components.js
@@ -11,6 +11,8 @@ export class Range extends EventTarget {
   }
 
   setupHtml(el, toString = (x) => x.toString()) {
+    el = findElement(el);
+
     const containerEl = document.createElement('label');
     this.containerEl = containerEl;
     el.append(containerEl);
@@ -107,6 +109,8 @@ export class Toggle extends EventTarget {
   }
 
   setupHtml(el) {
+    el = findElement(el);
+
     const containerEl = document.createElement('label');
     this.containerEl = containerEl;
     el.append(containerEl);
@@ -180,7 +184,7 @@ export class Selector extends EventTarget {
   }
 
   setupHtml(el) {
-    this.optionEls = [];
+    el = findElement(el);
 
     for (let i = 0; i < this.options.length; i++) {
       const option = this.options[i];
@@ -235,5 +239,13 @@ export class Selector extends EventTarget {
 
       this.dispatchEvent(new CustomEvent('change'));
     }
+  }
+}
+
+function findElement(el) {
+  if (el instanceof Element) {
+    return el;
+  } else {
+    return document.querySelector(el);
   }
 }
