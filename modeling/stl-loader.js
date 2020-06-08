cb56a860ed6662a8e86c4538f30d0255ff1f832e
Jacek Kopecky <jacek@jacek.cz>
2020-06-08 20:50:07 +0100

    review

--- modeling/stl-loader.js
+++ modeling/stl-loader.js
@@ -1,11 +1,11 @@
 import { STLLoader } from '../lib/STLLoader.js';
-import { Point3, Patch3, Surface3, Spectra, Instance, Vertex3 } from '../radiosity/index.js';
+import { Point3, Patch3, Surface3, Instance, Vertex3 } from '../radiosity/index.js';
 
 const LOADER = new STLLoader();
 
-export async function loadSTL(filepath, colorRGB) {
+export async function loadSTL(filepath, reflectance, emittance) {
   const geo = await load(filepath);
-  return STLToInstance(geo, colorRGB);
+  return stlToInstance(geo, reflectance, emittance);
 }
 
 function load(filepath) {
@@ -14,10 +14,10 @@ function load(filepath) {
   });
 }
 
-function STLToInstance(g, c) {
+function stlToInstance(g, reflectance, emittance) {
   // Vertices
   const vertices = [];
-  const posArray = g.attributes.position.array;
+  const posArray = g.getAttribute('position').array;
   for (let i = 0; i < posArray.length; i += 3) {
     vertices.push(
       new Vertex3(
@@ -45,18 +45,8 @@ function STLToInstance(g, c) {
   }
 
   // Surfaces
-  const surfaces = [
-    new Surface3(
-      new Spectra(
-        c[0] / 255,
-        c[1] / 255,
-        c[2] / 255,
-      ),
-      null,
-      patches,
-    ),
-  ];
+  const surface = new Surface3(reflectance, emittance, patches);
 
   // Instance
-  return new Instance(surfaces);
+  return new Instance([surface]);
 }
